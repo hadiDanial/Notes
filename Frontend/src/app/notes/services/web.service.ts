1328e8aed2c78a7a92e1bbd7649e510866d8e748
Hadi <hadidanial97@gmail.com>
2021-09-06 08:29:52 +0300

    Update web.service.ts

--- Frontend/src/app/notes/services/web.service.ts
+++ Frontend/src/app/notes/services/web.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class WebService {
+  ROOT_URL = "http://localhost:8084/"
+  constructor(private httpClient: HttpClient) { }
+  config = { headers: WebService.httpHeaders };
+  static httpHeaders = new HttpHeaders({
+    "Content-Type": "aplication/json",
+    'Accept': 'application/json'
+  });
 
-  constructor() { }
+
+
+  private handleError(error: HttpErrorResponse)
+  {
+    // Handle the HTTP error here
+    return throwError('Something wrong happened');
+  }
+}
+class NumbineHttpParamsGenerator
+{
+  public static insertParams(map: Map<string, any>): HttpParams
+  {
+    const params = new HttpParams();
+    if (map != null)
+    {
+      for (let [key, value] of map)
+      {
+        params.set(key, value);
+      }
+    }
+    return params;
+  }
 }
