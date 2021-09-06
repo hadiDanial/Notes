bd7936e4b5d43e814429383d0d745a884a3ae82a
Hadi <hadidanial97@gmail.com>
2021-09-06 11:50:27 +0300

    Update NoteController.java
    CrossOrigin

--- Backend/src/main/java/org/tsofen/Notes/controllers/NoteController.java
+++ Backend/src/main/java/org/tsofen/Notes/controllers/NoteController.java
@@ -3,6 +3,7 @@ package org.tsofen.Notes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.tsofen.Notes.beans.Note;
 
 @RestController
 @RequestMapping("notes")
+@CrossOrigin
 public class NoteController
 {
 	@Autowired
