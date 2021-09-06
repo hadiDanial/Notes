27058a6718579f4b06fa59f927f433534a3b4be1
Hadi <hadidanial97@gmail.com>
2021-09-07 00:36:33 +0300

    Update alert.service.ts

--- Frontend/src/app/notes/services/alert.service.ts
+++ Frontend/src/app/notes/services/alert.service.ts
@@ -8,7 +8,65 @@ import swal, { SweetAlertOptions } from 'sweetalert2';
 export class AlertService {
 
   constructor() { }
+  confirmAlert(
+    title: string,
+    message: string,
+    successMessage: string,
+    successTitle: string,
+    failureMessage: string,
+    failureTitle: string,
+    successCallback: Function,
+    failureCallback: Function,
+    dismissFunction?: Function,
+  ) 
+  {
+    const options = {
+      title: title,
+      text: message,
+      icon: 'warning',
+      showConfirmButton: true,
+      showCancelButton: true,
+      customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger',
+            title: '',
+          }
+    } as SweetAlertOptions;
 
+    swal.fire(options).then((result) =>
+    {
+      if (result.value)
+      {
+        successCallback();
+        if (!(successTitle == "" && successMessage == ""))
+        {
+          const successOptions = {
+            title : successTitle,
+            text: successMessage,
+            icon: 'success'
+          } as SweetAlertOptions;
+          swal.fire(successOptions);
+        }
+      }
+      else if (result.dismiss === swal.DismissReason.cancel)
+      {
+        failureCallback();
+        if (!(failureTitle == "" && failureMessage == ""))
+        {
+          const failureOptions = {
+            title : failureTitle,
+            text: failureMessage,
+            icon: 'error'
+          } as SweetAlertOptions;
+          swal.fire(failureOptions);
+        }
+      } else
+      {
+        if (dismissFunction != undefined)
+          dismissFunction();
+      }
+    });
+  }
   /**
    * 
    * @param title 
@@ -28,6 +86,32 @@ export class AlertService {
      } as SweetAlertOptions;
      
      swal.fire(options);
+   }
+ 
+  /**
+   * 
+   * @param title 
+   * @param timer 
+   * @param position 'top', 'top-start', 'top-end' (default position), 'center', 'center-start', 'center-end', 'bottom', 'bottom-start', or 'bottom-end'.
+   * @param icon 'success', 'error', 'info', 'warning', 'question'
+   */
+   alertWithCallback(title: string, timer: number, toast: boolean, successCallback: Function, position: string = 'top-end', icon: string = 'success')
+   {
+     const options = {
+       title: title,
+       icon: icon,
+       position: position,
+       showConfirmButton: false,
+       toast: toast,
+       timer: timer
+     } as SweetAlertOptions;
+     
+     swal.fire(options).then((result)=>
+     {
+
+         successCallback();
+       
+     });
    }
  
    loadingMenu(message: string, obs: Observable<boolean>, successCallback: Function, failureCallback: Function)
