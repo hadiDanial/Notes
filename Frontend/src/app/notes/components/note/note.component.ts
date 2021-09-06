5d938cadce34465844522d3944ea38019e641f0f
Hadi <hadidanial97@gmail.com>
2021-09-06 11:50:36 +0300

    Note

--- Frontend/src/app/notes/components/note/note.component.ts
+++ Frontend/src/app/notes/components/note/note.component.ts
@@ -7,22 +7,49 @@ import { Note } from '../../models/Note';
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.scss']
 })
-export class NoteComponent implements OnInit {
+export class NoteComponent implements OnInit
+{
   note: Note = new Note();
   editing = false;
   backgroundColor = "background-color: " + Color.Grey;
   constructor() { }
 
-  ngOnInit(): void {
-    this.updateBackgroundColor();
-  }
-
-  updateBackgroundColor()
+  ngOnInit(): void
   {
-    if(this.note != null && this.note.color != undefined)
+    if (this.note.color != undefined)
     {
-      this.backgroundColor = "background-color: " + this.note.color + ";"
+      this.updateBackgroundColor(this.note.color);
+    }
+    else
+    {
+      this.updateBackgroundColor(Color.Grey);
     }
   }
+  updateTitle(title: any)
+  {
+    this.note.title = title.target.value;
+  }
+  updateBody(body: any)
+  {
+    this.note.body = body.target.value;
+  }
+  updateBackgroundColor(color: Color)
+  {
 
+    this.note.color = color;
+    this.backgroundColor = "background-color: " + this.note.color + ";"
+
+  }
+  setPriority(priority:any)
+  {
+    this.note.priority = priority.value;
+  }
+  createNote()
+  {
+    if(this.note.title == "" || this.note.body == "")
+    {
+      
+    }
+    alert(JSON.stringify(this.note))
+  }
 }
