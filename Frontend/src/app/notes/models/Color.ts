e040e4758b35a73ab1b3531a7441265be3a874b3
Hadi <hadidanial97@gmail.com>
2021-09-06 11:50:17 +0300

    Models

--- Frontend/src/app/notes/models/Color.ts
+++ Frontend/src/app/notes/models/Color.ts
@@ -1,4 +1,4 @@
 export enum Color
 {
-    Grey = "#f2f2f2", Blue = "#a3a3ff",  Cream = "#fff7de", Lightred = "#ffb6a3", Green = "#b8ffb8", Pink = "#ffb8f3"
+    Grey = "#f2f2f2", Blue = "#a3a3ff",  Cream = "#fff7de", Lightred = "#ffb6a3", Green = "#b8ffb8", Pink = "#ffb8f3", Black = "#000000"
 }
--- Frontend/src/app/notes/models/Note.ts
+++ Frontend/src/app/notes/models/Note.ts
@@ -6,8 +6,9 @@ export class Note
     public static defaultColor: Color.Grey;
     public static defaultIcon: Icon.Normal;
     
-    constructor(public title?: string, public body?: string,
-                public priority?: number, public readFlag?: boolean,
-                public color?: Color, public icon?: Icon) { }
+    constructor(public title: string = "", public body: string = "",
+                public priority: number = 1, public readFlag: boolean = false,
+                public color: Color = Color.Grey, public icon: Icon = Icon.Normal) 
+                { }
     
 }
