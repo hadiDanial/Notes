bd5b7b93be285d51347c0cd6a531b85a107b4dd0
Hadi <hadidanial97@gmail.com>
2021-09-07 00:37:44 +0300

    Notes List updates

--- Frontend/src/app/notes/components/notes-list-item/notes-list-item.component.ts
+++ Frontend/src/app/notes/components/notes-list-item/notes-list-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Note } from '../../models/Note';
+import { AlertService } from '../../services/alert.service';
 import { NoteService } from '../../services/note.service';
 
 @Component({
@@ -7,22 +9,44 @@ import { NoteService } from '../../services/note.service';
   templateUrl: './notes-list-item.component.html',
   styleUrls: ['./notes-list-item.component.scss']
 })
-export class NotesListItemComponent implements OnInit {
+export class NotesListItemComponent implements OnInit
+{
 
   @Input()
   note: Note = new Note();
+  @Output()
+  emitter: EventEmitter<Note>= new EventEmitter<Note>();
   backgroundColor = "background-color: " + this.note.color;
-  constructor(private noteService: NoteService) { }
+  readStatus = (this.note.readFlag) ? "Read" : "Unread";
+  constructor(private noteService: NoteService, private router: Router, private alert: AlertService) { }
   
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.backgroundColor = "background-color: " + this.note.color;
+    this.readStatus = (this.note.readFlag) ? "Read" : "Unread";
   }
   viewNote()
   {
-
+    this.noteService.currentNote = this.note;
+    this.router.navigate(['/note/', { viewing: 'true' }]);
   }
   deleteNote()
   {
-    this.noteService.deleteNote(this.note.id);
+    if (this.note.id != undefined)
+    {
+      let obs = this.noteService.deleteNote(this.note.id);
+      obs.subscribe(val => 
+      {
+        if(val)
+        {
+          this.alert.alert("Note deleted", 1000, false, 'center', 'success');
+          this.emitter.emit(this.note);
+        }
+        else
+        {
+          this.alert.alert("Failed to delete...", 1000, false, 'center', 'error');
+        }
+      })
+    }
   }
 }
--- Frontend/src/app/notes/components/notes-list/notes-list.component.ts
+++ Frontend/src/app/notes/components/notes-list/notes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../../models/Note';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-notes-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes-list.component.scss']
 })
 export class NotesListComponent implements OnInit {
+  notes: Note[] = [];
 
-  constructor() { }
+  constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
+    let obs = this.noteService.getAllNotes();
+    obs.subscribe(notes => {
+      this.notes = notes;
+    })
   }
-
+  updateList(noteToRemove: Note)
+  {
+    let index = this.notes.indexOf(noteToRemove);
+    this.notes.splice(index, 1);
+  }
 }
